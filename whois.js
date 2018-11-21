7de0b355f0a3522a84a8edd030f53253efc53dbe
Gretten <shhworkmail@yandex.ru>
2018-11-21 18:50:46 +0300

    Log output formatted, regexps changed

--- whois.js
+++ whois.js
@@ -5,34 +5,37 @@ const express = require("express");
 const fs = require('fs');
 
 const app = express();
-const urlencodedParser = bodyParser.urlencoded({extended:false});
-const domainRegExp = /(?<=\/\/)[\w]+?\.[\w.]+/;
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const domainRegExp = /(?<=\/\/)[\w_-]+?\.[\w.]+/;
+const domain = /(domain|server)+/i;
 
 function whoisParser(arr, file) {
-  for (key of arr) {
-    whois.lookup(key, (err, data) => {
-      if (err) {
-        console.log(err);
-      }
-      let parsed = parser.parseWhoIsData(data);
-      for (key of parsed) {
-        if (key.value) {
-            fs.appendFileSync(file, `\r\n${key.value}`)
-        }
-      }
-    });
-  }
+    for (key of arr) {
+        whois.lookup(key, (err, data) => {
+            if (err) {
+                console.log(err);
+            }
+            let parsed = parser.parseWhoIsData(data);
+            let arrEnd = parsed.length;
+            for (key of parsed) {
+                if (key.attribute.match(domain)) {
+                    fs.appendFileSync(file, `${key.value}\r\n`);
+                }
+            }
+            fs.appendFileSync(file, `\r\n`);
+        });
+    }
 }
 
 function getArrayOfLinks(req, res) {
-    if(!req.body) return res.sendStatus(400);
-    if(!req.body.text) { 
-      console.log('nothing to find');
-      return;
+    if (!req.body) return res.sendStatus(400);
+    if (!req.body.text) {
+        console.log('nothing to find');
+        return;
     };
     let formResult = req.body.text.split('\r\n');
     let parsed = formResult.map(function(item) {
-      return item.match(domainRegExp)[0];
+        return item.match(domainRegExp)[0];
     });
     whoisParser(parsed, 'data.txt')
 };
